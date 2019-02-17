6cb75920207088e5430370030af4d4d19c317387
Morrison18 <35890626+Morrison18@users.noreply.github.com>
2019-02-17 11:56:20 +0000

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,24 @@
+function add() {
+  var result = 4 + 2;
+  alert("When the two numbers are added, the result is: " + result);
+}
+
+function subtract() {
+  var result = 4 - 2;
+  alert("When the two numbers are subtracted, the result is: " + result);
+  }
+
+function multiply() {
+  var result = 4 * 2;
+  alert("When the two numbers are multiplied, the result is: " + result);
+}
+
+function divide() {
+  var result = 4 / 2;
+  alert("When the two numbers are divided, the result is: " + result);
+}
+
+function modulus() {
+  var result = 4 % 2;
+  alert("When the two numbers are modulus, the result is: " + result);
+}
